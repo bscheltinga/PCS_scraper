42f0c2987a84b47e9222b70c70d4f3152e4dae44
Bouke Scheltinga <b.l.scheltinga@utwente.nl>
2023-06-24 18:10:11 +0200

    Finish tdf scraper based on PCS views last 7 days

--- tdf_2023.py
+++ tdf_2023.py
@@ -1,18 +1,33 @@
-import pandas as pd
 import requests
 from bs4 import BeautifulSoup
-import re
+import pandas as pd
 import numpy as np
 
-url = 'https://www.procyclingstats.com/rider/miguel-angel-lopez/statistics/overview'
-r = requests.get(url)
-soup = BeautifulSoup(r.content, "html.parser")
 
-# Find the bar diagram with visits
-views = []
-views_diagram = soup.find_all("div", {"class": "bg hide"})
-for item in views_diagram:
-    print(item)
-    views.append(int(item.contents[0]))
+def get_total_views_last_week(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, "html.parser")
 
-total_views_week = sum(views[-7:])  # takes the sum of the last 7 elements
+    # Find the bar diagram with visits
+    views = []
+    views_diagram = soup.find_all("div", {"class": "bg hide"})
+    for item in views_diagram:
+        views.append(int(item.contents[0]))
+
+    total_views_week = sum(views[-7:])  # takes the sum of the last 7 elements
+    return total_views_week
+
+
+# url = 'https://www.procyclingstats.com/rider/wout-van-aert/statistics/overview'
+# wva = get_total_views_last_week(url)
+
+# Load the starting list
+df = pd.read_excel('start_list.xlsx')  # This is now the Giro list.
+df_scrape = df.copy()
+df_scrape['views'] = np.zeros(len(df))
+
+for i in range(len(df)):
+    url = df['URL'][i][:-1] + str('/statistics')
+    df_scrape['views'][i] = get_total_views_last_week(url)
+
+df_scrape = df_scrape.sort_values('views', ascending=False)
