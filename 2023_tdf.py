9104d84301b7ff962558c394d8bdc85f493c4fae
Bouke Scheltinga <b.l.scheltinga@utwente.nl>
2024-04-10 11:15:42 +0200

    Add 2023 tdf

--- 2023_tdf.py
+++ 2023_tdf.py
@@ -14,18 +14,19 @@ def get_total_views_last_week(url):
     for item in views_diagram:
         views.append(int(item.contents[0]))
 
-    total_views_week = sum(views[-7:])  # takes the sum of the last 7 elements
+    total_views_week = sum(views[-28:])  # takes the sum of the last 28 elements
     return total_views_week
 
 
 # Load the starting list
-df = pd.read_excel(r'data/2023_06_26_tdf.xlsx')
+df = pd.read_excel(r'data/2023_06_27_tdf.xlsx')
 df_scrape = df.copy()
 df_scrape['views'] = np.zeros(len(df))
 
 for i in range(len(df)):
     url = df['Link'][i] + str('/statistics')
+    print('Scraping rider %i' %i)
     df_scrape['views'][i] = get_total_views_last_week(url)
 
 df_scrape = df_scrape.sort_values('views', ascending=False)
-# df_scrape.to_excel('data/2023_tdf_scraped.xlsx', index=False)
+df_scrape.to_excel('data/2023_06_30_tdf_scraped_28_days.xlsx', index=False)
